3864be756f050e9975ee2e7465999b177cd5eebc
wj <13981641699@163.com>
2016-11-22 21:31:14 +0800

    测试3

--- app/src/main/java/com/wj/mobilesafe/activity/Setup1Activity.java
+++ app/src/main/java/com/wj/mobilesafe/activity/Setup1Activity.java
@@ -1,7 +1,9 @@
 package com.wj.mobilesafe.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.wj.mobilesafe.R;
 
@@ -10,5 +12,12 @@ public class Setup1Activity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup1);
+    }
+
+    //下一页的点击事件
+    public void nextPage(View view){
+        Intent intent = new Intent(this,Setup2Activity.class);
+        startActivity(intent);
+        finish();
     }
 }
---
+++ app/src/main/java/com/wj/mobilesafe/activity/Setup2Activity.java
@@ -0,0 +1,14 @@
+package com.wj.mobilesafe.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.wj.mobilesafe.R;
+
+public class Setup2Activity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup2);
+    }
+}
