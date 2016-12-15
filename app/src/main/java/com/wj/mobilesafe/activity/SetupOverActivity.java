9e33fa8e808153fb25f84168224af4a52e3e940c
wj <13981641699@163.com>
2016-12-15 21:45:53 +0800

    SetupOverActivity界面编写

--- app/src/main/java/com/wj/mobilesafe/activity/SetupOverActivity.java
+++ app/src/main/java/com/wj/mobilesafe/activity/SetupOverActivity.java
@@ -3,12 +3,18 @@ package com.wj.mobilesafe.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.wj.mobilesafe.R;
 import com.wj.mobilesafe.utils.ConstantValue;
 import com.wj.mobilesafe.utils.SpUtil;
 
 public class SetupOverActivity extends Activity{
+
+    private TextView tv_phone;
+    private TextView tv_reset_setup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,11 +23,32 @@ public class SetupOverActivity extends Activity{
         if (setup_over) {
             //密码输入成功，并且4个设置页面完成--->停留在设置完成功能列表页面。
             setContentView(R.layout.activity_setup_over);
+
+            initUI();
+
         }else{
             //密码输入成功，4个设置页面未完成则跳转到设置页面一。
             Intent intent = new Intent(this,Setup1Activity.class);
             startActivity(intent);
             finish();
         }
+    }
+
+    private void initUI() {
+        tv_phone = (TextView) findViewById(R.id.tv_phone);
+        //设置联系人号码
+        String phone = SpUtil.getString(this, ConstantValue.CONTACT_PHONE, "");
+        tv_phone.setText(phone);
+
+        //重新设置条目被点击
+        tv_reset_setup = (TextView) findViewById(R.id.tv_reset_setup);
+        tv_reset_setup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Setup1Activity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
